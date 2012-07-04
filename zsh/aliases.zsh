5a9868ccbb57a26aafc41927b682e091f8f42b76
Revath S Kumar <revath@mobme.in>
2012-07-04 15:38:48 +0530

    added alias for subl and myip

--- zsh/aliases.zsh
+++ zsh/aliases.zsh
@@ -2,5 +2,5 @@ alias reload!='. ~/.zshrc'
 alias db='mysql -uroot -proot'
 alias al='tail -f /var/log/apache2/error.log'
 alias rc='rails console'
-alias ll='ls -al --color'
-alias ls='ls --color'
+alias subl='subl -a '
+alias myip="curl -s icanhazip.com"
