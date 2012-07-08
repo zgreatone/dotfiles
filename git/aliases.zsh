30b9ff98c6d3a8336270804445d5b057eee56327
Revath S Kumar <rsk@revathskumar.com>
2012-07-08 20:44:07 +0530

    added -m option to gc alias for git commit

--- git/aliases.zsh
+++ git/aliases.zsh
@@ -11,7 +11,7 @@ alias gl='git pull --prune'
 alias glog="git log --graph --pretty=format:'%Cred%h%Creset %an: %s - %Creset %C(yellow)%d%Creset %Cgreen(%cr)%Creset' --abbrev-commit --date=relative"
 alias gp='git push origin HEAD'
 alias gd='git diff'
-alias gc='git commit'
+alias gc='git commit -m'
 alias gca='git commit -a'
 alias gco='git checkout'
 alias gb='git branch'
