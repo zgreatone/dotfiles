45aa340b3710935c3307a442afc68d6499b63cb9
Revath S Kumar <rsk@revathskumar.com>
2012-09-24 11:06:19 +0530

    removed subl alias

--- zsh/aliases.zsh
+++ zsh/aliases.zsh
@@ -2,6 +2,5 @@ alias reload!='. ~/.zshrc'
 alias db='mysql -uroot -proot'
 alias al='tail -f /var/log/apache2/error.log'
 alias rc='rails console'
-alias subl='subl -a '
 alias myip="curl -s icanhazip.com"
 alias bw='echo "scale=2; `curl  --progress-bar -w "%{speed_download}" http://speedtest.wdc01.softlayer.com/downloads/test10.zip -o test.zip` / 131072" | bc | xargs -I {} echo {}Mb\/s'
