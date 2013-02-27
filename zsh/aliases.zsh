93f2b6ee8a6a60296db9ea0193d9c9e6f737a16c
Revath S Kumar <rsk@revathskumar.com>
2013-02-27 19:02:37 +0530

    adding alias for st3

--- zsh/aliases.zsh
+++ zsh/aliases.zsh
@@ -4,3 +4,4 @@ alias al='tail -f /var/log/apache2/error.log'
 alias rc='rails console'
 alias myip="curl -s icanhazip.com"
 alias bw='echo "scale=2; `curl  --progress-bar -w "%{speed_download}" http://speedtest.wdc01.softlayer.com/downloads/test10.zip -o test.zip` / 131072" | bc | xargs -I {} echo {}Mb\/s'
+alias st3='~/sublime_text_3/sublime_text'
