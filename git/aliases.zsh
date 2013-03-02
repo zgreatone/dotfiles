908ed986c19309ca13fa372c11cc99c6148ac4b5
Igor Semenko <igor@zoomdata.com>
2013-03-02 11:47:58 +0200

    scripts cleanup

--- git/aliases.zsh
+++ git/aliases.zsh
@@ -10,6 +10,7 @@ fi
 alias gl='git pull --prune'
 alias glog="git log --graph --pretty=format:'%Cred%h%Creset %an: %s - %Creset %C(yellow)%d%Creset %Cgreen(%cr)%Creset' --abbrev-commit --date=relative"
 alias gp='git push origin HEAD'
+alias gpr='git pull --rebase'
 alias gd='git diff'
 alias gc='git commit -m'
 alias gca='git commit -a'
--- zsh/aliases.zsh
+++ zsh/aliases.zsh
@@ -3,4 +3,5 @@ alias db='mysql -uroot -proot'
 alias al='tail -f /var/log/apache2/error.log'
 alias rc='rails console'
 alias myip="curl -s icanhazip.com"
-alias bw='echo "scale=2; `curl  --progress-bar -w "%{speed_download}" http://speedtest.wdc01.softlayer.com/downloads/test10.zip -o test.zip` / 131072" | bc | xargs -I {} echo {}Mb\/s'
+
+alias zwl='tail -f /usr/local/apache-tomcat-7.0.34/logs/zoomdata-websocket.log'
