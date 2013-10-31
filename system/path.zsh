8759bb398b7301362144306973fc332c15124a01
Igor Semenko <igor@zoomdata.com>
2013-10-31 15:40:59 +0200

    Option-E as backward history search

--- system/path.zsh
+++ system/path.zsh
@@ -1,4 +1,4 @@
 CELLAR=/usr/local/Cellar
-export PATH="./:./bin:/usr/local/sbin:/usr/local/storm/bin:$CELLAR/phantomjs/1.7.0:/usr/local/bin:/usr/local/sbin:$DOTFILES/bin:/Users/igor/tools/pygments:$PATH"
+export PATH=".:./bin:/usr/local/sbin:/usr/local/storm/bin:$CELLAR/phantomjs/1.7.0:/usr/local/bin:/usr/local/sbin:$DOTFILES/bin:/Users/igor/tools/pygments:$PATH"
 
 export MANPATH="/usr/local/man:/usr/local/mysql/man:/usr/local/git/man:$MANPATH"
