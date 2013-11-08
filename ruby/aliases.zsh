24fd38095bc6beac7393ae868e1fdd729e33c535
Igor Semenko <igor@zoomdata.com>
2013-11-08 12:11:54 +0200

    minor cleanup, added JAVA_HOME

--- ruby/aliases.zsh
+++
@@ -1,3 +0,0 @@
-alias r='rvm use 1.9.3-p194'
-
-alias migrate='rake db:migrate db:test:clone'
--- zsh/env.zsh
+++ zsh/env.zsh
@@ -1,2 +1,4 @@
 export EDITOR='subl'
 export SLIMERJSLAUNCHER=/Applications/Firefox.app/Contents/MacOS/firefox
+
+export JAVA_HOME=/Library/Java/JavaVirtualMachines/CurrentJDK/Contents/Home
