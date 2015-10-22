a6014b593ac5b8835e174dfb1ffa5d5feca61f08
Igor Semenko <igor@zoomdata.com>
2015-10-22 08:50:30 +0300

    removing git completion

--- docker/env.zsh
+++ docker/env.zsh
@@ -1,3 +1,4 @@
-export DOCKER_HOST=tcp://192.168.59.103:2376
-export DOCKER_CERT_PATH=~/.boot2docker/certs/boot2docker-vm
+export DOCKER_HOST=tcp://192.168.99.100:2376
+export DOCKER_MACHINE_NAME=default
 export DOCKER_TLS_VERIFY=1
+export DOCKER_CERT_PATH=/Users/igor/.docker/machine/machines/default
